a7776a8784949ef53cc2928e84a0fbd701adc084
ayushrjc <ayushrajchoudhary@gmail.com>
2024-02-21 17:51:06 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+from typing import List
 
-def get_requierements(file_path:str)->List[str]:
-    requirements = []
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for re in requirements]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DIT in requirements:
+        if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
-setip(
 
-    name = "Xray",
-    version = "0.0.1",
-    author = "Ayush Raj Choudhary",
-    author_email = "ayushrajchoudhary@gmail.com",
-    install_requires=get_requirements(),
-    package=find_packages()
+setup(
+
+name="Xray",
+version="0.0.1",
+author="Ayush Raj Choudhary",
+author_email="ayushrajchoudhary@gmail.com",
+install_requires=get_requirements(r"C:\\Users\\achoudhary\\Desktop\\DLproject\\requirements_dev.txt"),
+package=find_packages()
 
 )
